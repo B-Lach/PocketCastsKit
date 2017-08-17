a2cba21b6a696d0bf00ea53a846c1ca533942d05
Benny Lach <benny@discollect.me>
2017-08-17 15:12:59 +0200

    Added JSONParser

---
+++ PocketCastsKit/Helper/JSONParser.swift
@@ -0,0 +1,43 @@
+//
+//  JSONParser.swift
+//  PocketCastsKit
+//
+//  Created by Benny Lach on 17.08.17.
+//  Copyright © 2017 Benny Lach. All rights reserved.
+//
+
+import Foundation
+
+private struct ParserConstants {
+    static let dateFormat: String = "yyyy-MM-dd' 'HH:mm:ss"
+}
+
+struct JSONParser {
+    static let shared = JSONParser()
+    
+    var customDateFormatter: DateFormatter?
+    
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = ParserConstants.dateFormat
+        
+        return formatter
+    }
+    
+    private init() {}
+}
+
+// MARK: Decode Data to Object
+extension JSONParser {
+    func decode<T: Decodable>(_ data: Data, type: T.Type) -> T? {
+        let formatter = customDateFormatter ?? dateFormatter
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        guard let object = try? decoder.decode(type, from: data) else {
+            return nil
+        }
+        return object
+    }
+}
