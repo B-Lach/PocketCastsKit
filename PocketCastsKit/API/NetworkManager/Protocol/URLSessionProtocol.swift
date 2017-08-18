a5185111d3d7b7bc22c6ae838f144d3dfc1af4a1
Benny Lach <benny@discollect.me>
2017-08-18 22:12:22 +0200

    Added protocol for URLSession stubing

---
+++ PocketCastsKit/API/NetworkManager/Protocol/URLSessionProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  URLSessionProtocol.swift
+//  PocketCastsKit
+//
+//  Created by Benny Lach on 18.08.17.
+//  Copyright © 2017 Benny Lach. All rights reserved.
+//
+
+import Foundation
+
+typealias handler = (Data?, URLResponse?, Error?) -> Void
+
+protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol {}
+
