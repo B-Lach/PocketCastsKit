8b3821d90ac2404c9ad3bf2c74062d563140a282
Benny Lach <benny@discollect.me>
2017-08-21 11:41:46 +0200

    Missing PCKPodcast model changes

--- PocketCastsKit/Model/PCKPodcast.swift
+++ PocketCastsKit/Model/PCKPodcast.swift
@@ -13,7 +13,10 @@ public struct PCKPodcast {
     public let uuid: UUID
     public let url: URL
     public let title: String
+    public let category: String
     public let description: String
+    public let mediaType: String
+    public let language: String
     public let thumbnail: URL
     public let author: String
     public let sortOrder: Int
@@ -25,7 +28,10 @@ extension PCKPodcast: Decodable {
         case uuid
         case url
         case title
+        case category
         case description
+        case mediaType = "media_type"
+        case language
         case thumbnail = "thumbnail_url"
         case author
         case sortOrder = "episodes_sort_order"
@@ -34,13 +40,16 @@ extension PCKPodcast: Decodable {
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         
-        id = try values.decode(Int.self, forKey: .id)
+        id = try values.decodeIfPresent(Int.self, forKey: .id) ?? -1
         uuid = try values.decode(UUID.self, forKey: .uuid)
         url = try values.decode(URL.self, forKey: .url)
         title = try values.decode(String.self, forKey: .title)
+        category = try values.decodeIfPresent(String.self, forKey: .category) ?? "undefined"
         description = try values.decode(String.self, forKey: .description)
+        mediaType = try values.decodeIfPresent(String.self, forKey: .mediaType) ?? "undefined"
+        language = try values.decodeIfPresent(String.self, forKey: .language) ?? "undefined"
         thumbnail = try values.decode(URL.self, forKey: .thumbnail)
         author = try values.decodeIfPresent(String.self, forKey: .author) ?? "undefined"
-        sortOrder = try values.decode(Int.self, forKey: .sortOrder)
+        sortOrder = try values.decodeIfPresent(Int.self, forKey: .sortOrder) ?? -1
     }
 }
