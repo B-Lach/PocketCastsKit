000a120ac87dc43b24ba00933f7249046f54dc22
Benny Lach <benny@discollect.me>
2017-08-21 11:34:35 +0200

    Added optional Podcast properties

--- PocketCastsKitTests/Model/PCKPodcastTests.swift
+++ PocketCastsKitTests/Model/PCKPodcastTests.swift
@@ -15,22 +15,29 @@ class PCKPodcastTests: XCTestCase {
     let uuid = UUID(uuidString: "00414e50-2610-012e-05ba-00163e1b201c")!
     let url = URL(string: "https://ninjalooter.de")!
     let title = "Ninjalooter.de"
+    let category = "Technology"
     let desc =  "Der Podcast der Ninjalooter befasst sich mit Spielen jeglicher Art. Insbesondere MMOs, Beta-Eindrücke und PC-Rollen- und Strategiespiele stehen auf der Debattierliste."
+    let mediaType = "Audio"
+    let language = "de"
     let thumbnail = URL(string: "http://ninjalooter.de/blog/wp-content/uploads/NinjaCast_Logo.jpg")!
     let author = "Ninjalooter.de"
     let sortOrder = 3
     
     
     func testInitFromValues() {
-        let podcast = PCKPodcast(id: id, uuid: uuid, url: url,
-                                 title: title, description: desc,
-                                 thumbnail: thumbnail, author: author, sortOrder: sortOrder)
+        let podcast = PCKPodcast(id: id, uuid: uuid, url: url, title: title,
+                                 category: category, description: desc, mediaType: mediaType,
+                                 language: language, thumbnail: thumbnail,
+                                 author: author, sortOrder: sortOrder)
         
         XCTAssertEqual(podcast.id, id)
         XCTAssertEqual(podcast.uuid, uuid)
         XCTAssertEqual(podcast.url, url)
         XCTAssertEqual(podcast.title, title)
+        XCTAssertEqual(podcast.category, category)
         XCTAssertEqual(podcast.description, desc)
+        XCTAssertEqual(podcast.mediaType, mediaType)
+        XCTAssertEqual(podcast.language, language)
         XCTAssertEqual(podcast.thumbnail, thumbnail)
         XCTAssertEqual(podcast.author, author)
         XCTAssertEqual(podcast.sortOrder, sortOrder)
@@ -40,26 +47,32 @@ class PCKPodcastTests: XCTestCase {
         
         let decoder = TestHelper.Decoding.jsonDecoder
         
-        var podcast = try decoder.decode(PCKPodcast.self, from: TestHelper.TestData.podcastDataWithAuthor)
+        var podcast = try decoder.decode(PCKPodcast.self, from: TestHelper.TestData.podcastDataOptionalPresent)
         
         XCTAssertEqual(podcast.id, id)
         XCTAssertEqual(podcast.uuid, uuid)
         XCTAssertEqual(podcast.url, url)
         XCTAssertEqual(podcast.title, title)
+        XCTAssertEqual(podcast.category, category)
         XCTAssertEqual(podcast.description, desc)
+        XCTAssertEqual(podcast.mediaType, mediaType)
+        XCTAssertEqual(podcast.language, language)
         XCTAssertEqual(podcast.thumbnail, thumbnail)
         XCTAssertEqual(podcast.author, author)
         XCTAssertEqual(podcast.sortOrder, sortOrder)
-        // author can be null, seperate test case
-        podcast = try decoder.decode(PCKPodcast.self, from: TestHelper.TestData.podcastDataWithoutAuthor)
+        // Optional keys may not be present - new test case
+        podcast = try decoder.decode(PCKPodcast.self, from: TestHelper.TestData.podcastDataOptionalMissing)
         
-        XCTAssertEqual(podcast.id, id)
+        XCTAssertEqual(podcast.id, -1)
         XCTAssertEqual(podcast.uuid, uuid)
         XCTAssertEqual(podcast.url, url)
         XCTAssertEqual(podcast.title, title)
+        XCTAssertEqual(podcast.category, "undefined")
         XCTAssertEqual(podcast.description, desc)
+        XCTAssertEqual(podcast.mediaType, "undefined")
+        XCTAssertEqual(podcast.language, "undefined")
         XCTAssertEqual(podcast.thumbnail, thumbnail)
         XCTAssertEqual(podcast.author, "undefined")
-        XCTAssertEqual(podcast.sortOrder, sortOrder)
+        XCTAssertEqual(podcast.sortOrder, -1)
     }
 }
--- PocketCastsKitTests/TestHelper.swift
+++ PocketCastsKitTests/TestHelper.swift
@@ -181,14 +181,17 @@ extension TestHelper {
             """.data(using: .utf8)!
         }
         // Podcast test data
-        static var podcastDataWithAuthor: Data {
+        static var podcastDataOptionalPresent: Data {
             return """
                 {
                     "id": 5810,
                     "uuid": "00414e50-2610-012e-05ba-00163e1b201c",
                     "url": "https://ninjalooter.de",
                     "title": "Ninjalooter.de",
+                    "category": "Technology",
                     "description": "Der Podcast der Ninjalooter befasst sich mit Spielen jeglicher Art. Insbesondere MMOs, Beta-Eindrücke und PC-Rollen- und Strategiespiele stehen auf der Debattierliste.",
+                    "media_type": "Audio",
+                    "language": "de",
                     "thumbnail_url": "http://ninjalooter.de/blog/wp-content/uploads/NinjaCast_Logo.jpg",
                     "author": "Ninjalooter.de",
                     "episodes_sort_order": 3
@@ -196,17 +199,14 @@ extension TestHelper {
             """.data(using: .utf8)!
         }
         
-        static var podcastDataWithoutAuthor: Data {
+        static var podcastDataOptionalMissing: Data {
             return """
                 {
-                    "id": 5810,
                     "uuid": "00414e50-2610-012e-05ba-00163e1b201c",
                     "url": "https://ninjalooter.de",
                     "title": "Ninjalooter.de",
                     "description": "Der Podcast der Ninjalooter befasst sich mit Spielen jeglicher Art. Insbesondere MMOs, Beta-Eindrücke und PC-Rollen- und Strategiespiele stehen auf der Debattierliste.",
                     "thumbnail_url": "http://ninjalooter.de/blog/wp-content/uploads/NinjaCast_Logo.jpg",
-                    "author": null,
-                    "episodes_sort_order": 3
                 }
                 """.data(using: .utf8)!
         }
